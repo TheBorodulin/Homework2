f80e94d55d53dd4a0d09f8964cb157891e514b12
TheBorodulin <34696689+TheBorodulin@users.noreply.github.com>
2022-08-05 15:42:25 +0300

    Add files via upload

---
+++ src/test/java/JAVABASICPART2/Exercise5.java
@@ -0,0 +1,20 @@
+package JAVABASICPART2;
+
+public class Exercise5 {
+//
+//    String[] students = new String[10];
+//    String studentName = "Peter Parker";
+//    students[0] = studentName;
+//    studentName = null;
+//
+//
+//    After execution of line #1 the students reference points to an array of ten String
+//    references. Those references don't point to anything yet.
+//    After execution of line #2 the studentName reference points to string "Peter Parker".
+//    After execution of line #3 the first reference in the array also points to that same
+//    string.
+//    After execution of line #4 studentName doesn't point to anything anymore.
+//
+//    So at the end 'students' points to an array of ten references; all of them are null
+//    except for the first one: it points to a String "Peter Parker".
+}
