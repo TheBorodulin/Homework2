0121f68a070d11ec0f778f21fb8dec55aab111c8
TheBorodulin <34696689+TheBorodulin@users.noreply.github.com>
2022-08-05 15:46:39 +0300

    Add files via upload

---
+++ src/test/java/JAVABASICPART2/Exercise6.java
@@ -0,0 +1,6 @@
+package JAVABASICPART2;
+
+public class Exercise6 {
+//    A program does not explicitly destroy objects. A program can set all references to an object to null so that the becomes eligible for garbage collection.
+//    But the program does not actually destroy objects.
+}
